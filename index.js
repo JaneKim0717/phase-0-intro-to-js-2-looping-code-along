2a9ecbb5c758bb38c87f6754832b31fea1d91d93
Jane Kim <jane.kim0717@gmail.com>
2022-01-13 23:36:32 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+
+
+function writeCards(name, event){
+    let newArray = [];
+    for (let i = 0; i < name.length; i++) {
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return newArray;
+}
+
+writeCards("names", "event")
+
+function countDown(init = 10) {
+    while (init>=0) {
+        console.log(init--);
+    }
+}
