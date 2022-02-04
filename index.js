0400c5ca13d0872ce9f8c4eae086bfba73b79efd
Jane Kim <jane.kim0717@gmail.com>
2022-02-03 20:46:46 -0500

    commit tree 76cf02c1414ca6d39ec22f22373736e6032ff4ff

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+// const names = ["Guadalupe", "Ollie", "Aki"];
+// const event = "surprise";
+
+
+// function writeCards(name, event){
+//     let newArray = [];
+//     for (let i = 0; i < name.length; i++) {
+//         newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+//         debugger;
+//     }
+//     return newArray;
+// }
+
+// writeCards("names", "event")
+
+// function countDown(init = 10) {
+//     while (init>=0) {
+//         console.log(init--);
+//     }
+// }
+
+
+
+
 const names = ["Guadalupe", "Ollie", "Aki"];
 const event = "surprise";
 
-
-function writeCards(name, event){
+function writeCards (names, event) {
     let newArray = [];
-    for (let i = 0; i < name.length; i++) {
-        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
-        debugger;
+    for (let i = 0; i < names.length; i++) {
+        newArray.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
     }
     return newArray;
-}
+} 
 
-writeCards("names", "event")
+// function countDown (number) {
+//     let number = 10;
+//     while (number > 0) {
+//         console.log (number);
+//         number --; 
+//     }
+// }
 
-function countDown(init = 10) {
-    while (init>=0) {
-        console.log(init--);
+function countDown(number = 10) {
+    while (number >= 0) {
+        console.log(number--);
     }
 }
+
+console.log (countDown(10));
